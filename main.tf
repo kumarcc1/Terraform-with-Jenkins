3ae699bdfe863259d5a5868544bb866f98321393
kumara c <108408924+kumarcc1@users.noreply.github.com>
2023-07-21 17:46:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,59 +1,17 @@
 provider "aws" {
-
-region = "us-east-1"
+        region = "us-east-1"
 }
 
-locals {
-
-USA  = {
-
-name = "usa-project"
-owner = "kumara"
+resource "aws_instance" "sanvi" {
+        instance_type   =  "t2.micro"
+        ami = "ami-07dff4fe919dee33e"
+        tags = {
+        Name = "sanvi"
  }
 }
-
-locals {
-  UK  = {
-  Name = "UK-Project"
-  owner = " kumara"
+resource "aws_vpc" "main" {
+ cidr_block = "172.16.0.0/16"
+ tags = {
+   name = "main"
  }
-}
-
-resource "aws_instance" "web" {
-        instance_type   = "t2.micro"
-        ami             = "ami-07dff4fe919dee33e"
-        tags            = local.USA
-}
-
-resource "aws_vpc" "VPC" {
-
-        cidr_block      = "10.0.0.0/16"
-        instance_tenancy = "default"
-        tags            = local.USA
-}
-
-resource "aws_ebs_volume" "vol1" {
-availability_zone       = "us-east-1a"
-size                    = 2
-tags                    = local.USA
-}
-
-
-resource "aws_instance" "webuk" {
-        instance_type   = "t2.micro"
-        ami             = "ami-07dff4fe919dee33e"
-        tags            = local.UK
-}
-
-resource "aws_vpc" "VPCUK" {
-
-        cidr_block      = "10.0.0.0/16"
-        instance_tenancy = "default"
-        tags            = local.UK
-}
-
-resource "aws_ebs_volume" "vol2" {
-availability_zone       = "us-east-1a"
-size                    = 2
-tags                    = local.UK
 }
