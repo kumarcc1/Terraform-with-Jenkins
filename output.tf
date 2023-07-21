a9b119dc8516ebb5422a42e3bbceb0262ec08340
kumara c <108408924+kumarcc1@users.noreply.github.com>
2023-07-21 17:44:38 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,12 +3,12 @@ output "instance_ip" {
 }
 
 output "instance_arn" {
-  value = aws_instance.arn
+  value = aws_instance.local.arn
   sensitive = true
 }
 
 output "instance_ami" {
-  value = aws_instance.ami
+  value = aws_instance.local.ami
 }
 
 output "vpc_cidr" {
