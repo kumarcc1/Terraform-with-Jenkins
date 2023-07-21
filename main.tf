c863f494cfa038bc9d38f078205febaf7bd17f61
kumara c <108408924+kumarcc1@users.noreply.github.com>
2023-07-21 18:00:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
         region = "us-east-1"
 }
 
-resource "aws_instance" "Server" {
+resource "aws_instance" "Server-01" {
         instance_type   =  "t2.micro"
         ami = "ami-07dff4fe919dee33e"
         tags = {
-        Name = "Server"
+        Name = "Server-01"
  }
 }
 resource "aws_vpc" "main" {
