f92943e222f5975ff4188fbe0744f785a89df1a2
kumara c <108408924+kumarcc1@users.noreply.github.com>
2023-07-21 18:00:05 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "instance_ip" {
-  value = aws_instance.sanvi.public_ip
+  value = aws_instance.Server-01.public_ip
 }
 
 output "instance_arn" {
-  value = aws_instance.sanvi.arn
+  value = aws_instance.Server-01.arn
   sensitive = true
 }
 
 output "instance_ami" {
-  value = aws_instance.sanvi.ami
+  value = aws_instance.Server-01.ami
 }
 
 output "vpc_cidr" {
